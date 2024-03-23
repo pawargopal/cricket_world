0ef7d66e6809af6a168d244dc18d310d155a5c8e
pawargopal <105573376+pawargopal@users.noreply.github.com>
2024-03-23 13:03:02 +0530

    Add files via upload
    project uploaded.

---
+++ cricket_fox/src/main/java/com/gp/app/cricket_fox/CricketFoxApplication.java
@@ -0,0 +1,13 @@
+package com.gp.app.cricket_fox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketFoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketFoxApplication.class, args);
+	}
+
+}
---
+++ cricket_fox/src/test/java/com/gp/app/cricket_fox/CricketFoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.app.cricket_fox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketFoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
